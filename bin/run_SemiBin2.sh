15e8fb391d964eaddb12d8a00d35efb421f1bab2
Sumeet Tiwari <sumeet.kumartt@gmail.com>
2023-12-29 18:49:15 +0000

    bin

---
+++ bin/run_SemiBin2.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# SemiBin2=v2.0.2
+# Script to run SemiBin2
+
+# Initialize variables
+CONTIG=""
+BAM=""
+OUTPUTDIR=""
+THREADS=1
+ENVIRONMENT=""
+
+# Function to display usage information
+usage() {
+    echo "Usage: $0 -c CONTIG -b BAM -e ENVIRONMENT -o OUTPUTDIR -t THREADS"
+    exit 1
+}
+
+# Function to check if a file exists
+check_file() {
+  if [ ! -f "$1" ]; then
+    echo "Error: File $1 not found" 
+    exit 1
+  fi
+}
+
+# Parse command-line arguments
+while getopts "c:b:e:o:t:" opt; do
+    case $opt in
+        c) CONTIG=$OPTARG ;;
+        b) BAM=$OPTARG ;;
+        e) ENVIRONMENT=$OPTARG ;;
+        o) OUTPUTDIR=$OPTARG ;;
+        t) THREADS=$OPTARG ;;
+        \?) usage ;;
+    esac
+done
+
+# Check if all the parameters are specified
+if [ -z "$CONTIG" ] || [ -z "$BAM" ] || [ -z "$OUTPUTDIR" ] || [ -z "$ENVIRONMENT" ]; then
+    echo "Error: $CONTIG, $BAM, $OUTPUTDIR and $ENVIRONMENT are required"
+    usage
+fi
+
+# Check input files exist
+check_file "$CONTIG"
+check_file "$BAM"
+
+# Run SemiBin2
+echo "Running SemiBin2..."
+SemiBin2 single_easy_bin \
+    --environment "$ENVIRONMENT" \
+    -i "$CONTIG" \
+    -b "$BAM" \
+    -o "$OUTPUTDIR"
+
+echo "SemiBin2 complete."
---
+++ bin/run_metaphlan4.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#Metaphlan4=v4.0.6
+# Script to run metaphlan4
+
+# Initialize variables
+FORWARD=""
+REVERSE=""
+OUTPUTDIR=""
+THREADS=1
+
+# Function to display usage information
+usage() {
+    echo "Usage: $0 -1 FORWARD -2 REVERSE -o OUTPUTDIR -t THREADS"
+    exit 1
+}
+
+# Parse command-line arguments
+while getopts "1:2:o:t:" opt; do
+    case $opt in
+        1) FORWARD=$OPTARG ;;
+        2) REVERSE=$OPTARG ;;
+        o) OUTPUTDIR=$OPTARG ;;
+        t) THREADS=$OPTARG ;;
+        *) usage ;;
+    esac
+done
+
+# Check if all the parameters are specified
+if [ -z "$FORWARD" ] || [ -z "$REVERSE" ] || [ -z "$OUTPUTDIR" ]; then
+    echo "Error: All parameters are required."
+    usage
+fi
+
+# Check if file input reads are found else exit
+if [ ! -e "$FORWARD" ] || [ ! -e "$REVERSE" ]; then
+    echo "Error: Missing $FORWARD and $REVERSE reads files. Exiting..."
+    exit 1
+fi
+
+echo "Both $FORWARD and $REVERSE found."
+
+# Merge the reads
+mkdir -p $OUTPUTDIR
+gzip -c -d $FORWARD >$OUTPUTDIR/reads.fq
+gzip -c -d $REVERSE >>$OUTPUTDIR/reads.fq
+
+# Image and database location
+IMAGE="/qib/platforms/Informatics/transfer/outgoing/singularity/core/metaphlan__4.0.6.simg"
+MPA="/qib/platforms/Informatics/transfer/outgoing/databases/humann_db/mpa/mpa_vOct22_CHOCOPhlAnSGB_202212"
+INDEX=$(cat $MPA/mpa_latest)
+echo "$INDEX"
+
+singularity exec $IMAGE metaphlan $OUTPUTDIR/reads.fq --input_type fastq \
+    -o $OUTPUTDIR/$(basename $OUTPUTDIR).tsv --force \
+    --bowtie2db $MPA -x $INDEX \
+    --nproc $THREADS --unclassified_estimation \
+    -t rel_ab
+
+rm $OUTPUTDIR/reads.fq
